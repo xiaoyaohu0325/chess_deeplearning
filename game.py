6e8f8d1706c23a65214ed3af0139f16f3307fd0c
Jian Wang <jian_esri@esri.com>
2017-11-04 08:13:44 +0800

    add checkpoint function

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import chess
+import chess.pgn as pgn
+import h5py as h5
 
 
 class Game(object):
@@ -9,15 +11,22 @@ class Game(object):
         }
         self.turn = chess.WHITE
         self.board = chess.Board()
+        self.game = pgn.Game()
+        self.game.headers["Event"] = "checkpoint"
+        self.game.headers["White"] = white_player.name
+        self.game.headers["Black"] = black_player.name
+        self.game_node = self.game
         self.is_game_over = False
 
     def play(self):
         if self.board.is_game_over(claim_draw=True):
+            self.game.headers["Result"] = self.board.result(claim_draw=True)
             return None
 
         fen = self.board.fen()
         move = self.players[self.turn].generate_move(fen)
         self.board.push(move)
+        self.game_node = self.game_node.add_variation(move)
         self.turn = not self.turn
         return move
 
@@ -32,3 +41,56 @@ class Game(object):
             return None
         else:
             raise 'game is not over'
+
+    def save_to_h5(self, file_path):
+        exporter = pgn.StringExporter(headers=True)
+        pgn_str = self.game.accept(exporter)
+
+        h5f = h5.File(file_path)
+        white_name = self.players[chess.WHITE].name
+        black_name = self.players[chess.BLACK].name
+
+        try:
+            # see http://docs.h5py.org/en/latest/high/group.html#Group.create_dataset
+            dt = h5.special_dtype(vlen=bytes)
+            if "draw" not in h5f:
+                h5f.require_dataset(
+                    name='draw',
+                    dtype=dt,
+                    shape=(0,),
+                    maxshape=(None,),
+                    chunks=True,
+                    compression="gzip")
+            if white_name not in h5f:
+                h5f.require_dataset(
+                    name=white_name,
+                    dtype=dt,
+                    shape=(0,),
+                    maxshape=(None,),
+                    chunks=True,
+                    compression="gzip")
+            if black_name not in h5f:
+                h5f.require_dataset(
+                    name=black_name,
+                    dtype=dt,
+                    shape=(0,),
+                    maxshape=(None,),
+                    chunks=True,
+                    compression="gzip")
+
+            winner_color = self.winner_color()
+            if winner_color == chess.WHITE:
+                dest = h5f[white_name]
+            elif winner_color == chess.BLACK:
+                dest = h5f[black_name]
+            else:
+                dest = h5f["draw"]
+
+            size = len(dest)
+            dest.resize((size + 1,))
+            dest[size] = pgn_str
+        except Exception as e:
+            print("append to hdf5 failed")
+            raise e
+        finally:
+            h5f.close()
---
+++ train_checkpoint.py
@@ -0,0 +1,35 @@
+from game import Game
+from player import AIPlayer
+from policy import ResnetPolicy
+import chess
+
+
+def play(white_player, black_player, n_game=100):
+    for i in range(n_game):
+        game = Game(white_player, black_player)
+        move = game.play()
+        while move is not None:
+            move = game.play()
+
+        game.save_to_h5("./out/checkpoint/gen0_gen1.h5")
+
+
+def run_checkpoint():
+    policy_1 = ResnetPolicy.load_model("./out/model.json")
+    policy_1.model.load_weights("./out/random_weights")
+    policy_2 = ResnetPolicy.load_model("./out/model.json")
+    policy_2.model.load_weights("./out/train/weights.00000.hdf5")
+
+    # assign black and white
+    p1 = AIPlayer('gen0', chess.WHITE, policy_1, simulation=40, depth=2)
+    p2 = AIPlayer('gen1', chess.BLACK, policy_2, simulation=40, depth=2)
+    play(p1, p2)
+
+    # switch color
+    p1 = AIPlayer('gen0', chess.BLACK, policy_1, simulation=40, depth=2)
+    p2 = AIPlayer('gen1', chess.WHITE, policy_2, simulation=40, depth=2)
+    play(p2, p1)
+
+
+if __name__ == '__main__':
+    run_checkpoint()
